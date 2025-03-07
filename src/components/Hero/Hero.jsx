19c80c61c1122c4b95e60406e9229b6d4a97fe41
Magret <belloabiodun17@gmail.com>
2025-03-07 01:11:22 -0330

    Implemented hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,18 @@ import "./Hero.scss";
 
 export default function Hero() {
     return (
-        <section>
-            <h1>Hero</h1>
+        <section className="hero">
+            <section className="hero__container">
+                <div className="hero__box">
+                    <p className="hero__texts">Loan Application Denied?</p>
+                    <p className="hero__texts">Try with Us</p>
+                </div>
+
+                <div className="hero__boxe">
+                    <p className="hero__text">You are one step away from accessing that loan you need</p>
+                    <button className="hero__button">Apply loan</button>
+                </div>
+            </section>
         </section>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,22 @@
 import "./Home.scss";
+import Header from "../../components/Header/Header";
+import Hero from "../../components/Hero/Hero";
+import Footer from "../../components/Footer/Footer";
+import { useState } from 'react';
 
 export default function Home() {
+    const [ ellipsisCLick, setEllipsisClick ] = useState(false);
+    
+    const handleEllipsisClick = () => {
+        setEllipsisClick(!ellipsisCLick);
+    }
+
     return (
         <section>
-            <h1>Home</h1>
+            <Header handleEllipsisClick={handleEllipsisClick} ellipsisCLick={ellipsisCLick}/>
+            <Hero ellipsisCLick={ellipsisCLick}/>
+            
+            <Footer />
         </section>
     )
 }
