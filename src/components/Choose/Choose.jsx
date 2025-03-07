476edc79f67a586b77df61473f067c7857ee1520
Magret <belloabiodun17@gmail.com>
2025-03-07 02:44:05 -0330

    Implement choose section to home page

--- src/components/Choose/Choose.jsx
+++ src/components/Choose/Choose.jsx
@@ -1,18 +1,28 @@
 import "./Choose.scss";
+import ChooseImage from "../../assets/images/money2.jpg";
 
 export default function Choose() {
     return (
-        <article>
-            <section>
-                <p>Why Choose Us</p>
-                <p>Your trusted partner for personalized loan solutions, expert financial guidance</p>
-                <img src="" alt="" />
+        <article className="choose">
+            <section className="choose__box choose__box-width">
+                <p className="choose__box-title">Why Choose Us</p>
+                <p className="choose__box-subtitle">Your trusted partner for personalized loan solutions, expert financial guidance</p>
+                <img className="choose__box-image" src={ChooseImage} alt="choose section image" />
             </section>
 
-            <section>
-                <div></div>
-                <div></div>
-                <div></div>
+            <section className="choose__box choose__box-container">
+                <div className="choose__box-content">
+                    <p className="choose__box-number">50+</p>
+                    <p className="choose__box-text">Years of Experience</p>
+                </div>
+                <div className="choose__box-content">
+                    <p className="choose__box-number">20k</p>
+                    <p className="choose__box-text">Loans Approved</p>
+                </div>
+                <div className="choose__box-content">
+                    <p className="choose__box-number">15k</p>
+                    <p className="choose__box-text">Satisfied Clients</p>
+                </div>
             </section>
         </article>
     )
