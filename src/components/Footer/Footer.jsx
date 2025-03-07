4c4a7b6965a539f9032ba9c3c3ada7ed091b6c8e
Magret <belloabiodun17@gmail.com>
2025-03-07 03:02:43 -0330

    Implemented footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,37 @@
 import "./Footer.scss";
+import LinkedInIcon from "../../assets/icons/linkedin.png";
+import TwitterIcon from "../../assets/icons/twitter.png";
 
 export default function Footer() {
     return (
-        <section>
-            <h1>Footer</h1>
-        </section>
-    )
+        <footer className="footer">
+            <div className="footer__content">
+
+                {/* Social Media Icons */}
+                <div className="footer__social">
+                    <a
+                        href="https://www.linkedin.com/in/oyedele-abiodun/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer__social-link"
+                    >
+                        <img className="footer__social-image" src={LinkedInIcon} alt="LinkedIn" />
+                    </a>
+                    <a
+                        href="https://x.com/OyedeleMagret"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer__social-link"
+                    >
+                        <img className="footer__social-image" src={TwitterIcon} alt="Twitter" />
+                    </a>
+                </div>
+
+                {/* Copyright Notice */}
+                <p className="footer__text">
+                    &copy; {new Date().getFullYear()} InstaloanX. All rights reserved.
+                </p>
+            </div>
+        </footer>
+    );
 }
