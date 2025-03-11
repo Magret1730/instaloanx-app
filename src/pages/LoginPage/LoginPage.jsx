e8cbfb5443701ba0a4304a4c938427deae518fe8
Magret <belloabiodun17@gmail.com>
2025-03-11 10:12:23 -0230

    Created the login page and made it responsive

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,16 @@
+import "./LoginPage.scss";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Login from "../../components/Login/Login";
+
+export default function LoginPage() {
+    return (
+        <section>
+            <Header />
+
+            <Login />
+
+            <Footer />
+        </section>
+    )
+}
