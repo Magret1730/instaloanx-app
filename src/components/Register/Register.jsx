ffadd866b30c734e1027cf498db358916f7d4204
Magret <belloabiodun17@gmail.com>
2025-03-12 01:05:11 -0230

    Adjusted success message color

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -58,7 +58,7 @@ export default function Register() {
 
     // Validate email
     const isEmailValid = (Email) => {
-        const emailRegex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         if (!Email) {
             setEmailError("This field is required");
